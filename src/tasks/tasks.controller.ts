b301931e8ff293e21b1396be34f8b14d3026c020
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-29 01:07:45 -0300

    added getTaskById

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {Task} from "./task.model";
 import {CreateTaskDto} from "./dto/create-task.dto"
@@ -12,6 +12,13 @@ export class TasksController {
     @Get()
     getAllTasks(): Task[]{
         return this.tasksService.getAllTasks();
+    }
+
+
+
+    @Get("/:id")
+    getTaskById(@Param("id") id: string): Task{
+        return this.tasksService.getTaskById(id);
     }
 
     @Post()
